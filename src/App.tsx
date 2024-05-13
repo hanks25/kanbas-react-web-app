e62cc44ebb94eefeeeb9ca9a4708ccac5126658d
hanks <chenhanks@gmail.com>
2024-05-13 18:57:12 -0400

    some change to delete logo:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import Labs from "./Labs";
 import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
