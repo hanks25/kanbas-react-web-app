70b261984894a59b0f00781dc8cc32e94f02f556
hanks <chenhanks@gmail.com>
2024-05-26 12:30:41 -0700

    finishing the lab

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,15 +5,14 @@ import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 export default function Labs() {
   return (
-    <div>
-      <h1>Han Chen</h1>
+    <div className="p-3">
       <h1>Labs</h1>
       <TOC />
       <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
+        <Route path="/" element={<Navigate to="Lab3" />} />
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3" element={<Lab3 />} />
+        <Route path="Lab3/*" element={<Lab3 />} />
       </Routes>
     </div>
   );
